50b1ea0381587d24aa39ce66ab98f41c7dffb84b
Grifith Sheeba Menon <Sheebam@ad.karanji.com>
2025-03-26 10:01:00 +0530

    updated code

--- Day5/first rows 1.py
+++ Day5/first rows 1.py
@@ -1,14 +1,17 @@
-rows = 5
+rows = 9
 
 for i in range( 0,rows):
     for j in range (0, rows):
         if( (i==0)or(j==0)or(i==rows-1)or(j==rows-1)):
             print("1", end=" ")
-        elif(): 
+        elif(i == rows // 2 and j == rows // 2): 
             print("1", end=" ")
         else:
             print("0", end=" ")
         
     print('')
+
+
+
     
     
--- practice/reverse pyramid.py
+++ practice/reverse pyramid.py
@@ -1,9 +1,7 @@
 rows = 5
 
-for i in range(rows,0,-1):
-    
-    for j in range (rows, 0 , -1):
-        
-        print (j, end="")
-    print( ' ' )
-    
+for i in range(rows):
+    print(" " * i, end="")  
+    for j in range(rows - i, 0, -1):
+        print(j, end="")
+    print()
